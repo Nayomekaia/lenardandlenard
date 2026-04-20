ab4e4ed66314e166c45347727c89e2fbf46d6ad1
Nayomekaia <nayomedoelwijt@gmail.com>
2026-04-21 01:57:59 +0200

    basic navigation styling

--- src/lib/organisms/NavBottom.svelte
+++ src/lib/organisms/NavBottom.svelte
@@ -48,3 +48,71 @@
 
   </ul>
 </nav>
+
+<style>
+
+/* Algemene navigatie styles */
+.desktop-nav{
+  display:flex;
+  justify-content: center;
+  margin: 0 auto;
+  border-radius: 5px;
+  background-color: black;
+  padding: 1rem;
+  width: 1000px;
+  gap: 5rem;
+  margin-top: 2rem;
+}
+
+li {
+  display: flex;
+  flex-direction: row;
+  list-style: none;
+  text-transform: uppercase;
+}
+
+details {
+  position: relative;
+  font-family: var(--font-body);
+  color: white;
+}
+
+.dropdown {
+  position: absolute;
+  top: 100%;
+  left: 0;
+  margin-top: 2rem;
+  list-style: none;
+  padding: 1.5em;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+details:not([open]) .dropdown {
+  display: none;
+}
+
+summary {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  cursor: pointer;
+  list-style: none; 
+  gap: 1rem;
+}
+
+summary::-webkit-details-marker {
+  display: none;
+}
+
+summary::after {
+  content: "▾";
+  transition: transform 0.2s ease;
+}
+
+details[open] summary::after {
+  transform: rotate(180deg);
+}
+
+</style>
