a66c96068167821d87549fb04179b8b2fe78321d
Nayomekaia <nayomedoelwijt@gmail.com>
2026-04-21 01:56:30 +0200

    basic layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,33 @@
 <script>
-	import Breadcrumb from '$lib/atoms/Breadcrumb.svelte';
-	import NavBottom from '$lib/organisms/NavBottom.svelte';
+	import Breadcrumb from "$lib/atoms/Breadcrumb.svelte";
+	import NavBottom from "$lib/organisms/NavBottom.svelte";
 
 	let { children } = $props();
 </script>
 
-
-
 <header>
-	<Breadcrumb />
 	<NavBottom />
+	<Breadcrumb />
 </header>
 
+<main>
 {@render children()}
+</main>
+
+<footer>
+</footer>
+
+<style>
+
+:global(*) {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+:global(body)  {
+		background-color: black;
+	}
+
+
+</style>
