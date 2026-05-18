8aacb0527c6a0271a4335f9045b1aa7b385c2489
Nayomekaia <nayomedoelwijt@gmail.com>
2026-05-18 14:00:10 +0200

    added scene
    added scene with three base enviroment

--- src/lib/three/LogoScene.svelte
+++ src/lib/three/LogoScene.svelte
@@ -2,4 +2,24 @@
     /* base */
     import { onMount } from "svelte";
     import * as THREE from "three";
+    let container;
+
+    onMount(() => {
+        // SCENE
+        const scene = new THREE.Scene();
+
+        // ENVIRONMENT
+        const envTexture = new THREE.CubeTextureLoader()
+            .setPath("https://threejs.org/examples/textures/cube/Bridge2/")
+            .load([
+                "posx.jpg",
+                "negx.jpg",
+                "posy.jpg",
+                "negy.jpg",
+                "posz.jpg",
+                "negz.jpg",
+            ]);
+
+        scene.environment = envTexture;
+        scene.background = null;
 </script>
