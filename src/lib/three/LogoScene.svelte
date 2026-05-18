372d5706a059e0946fe3b010c8e8b42dd9d65ca0
Nayomekaia <nayomedoelwijt@gmail.com>
2026-05-18 14:00:32 +0200

    added base lights

--- src/lib/three/LogoScene.svelte
+++ src/lib/three/LogoScene.svelte
@@ -22,4 +22,12 @@
 
         scene.environment = envTexture;
         scene.background = null;
+
+        // LIGHTS
+        const ambientLight = new THREE.AmbientLight(0xffffff, 1.5);
+        scene.add(ambientLight);
+
+        const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+        directionalLight.position.set(5, 5, 5);
+        scene.add(directionalLight);
 </script>
