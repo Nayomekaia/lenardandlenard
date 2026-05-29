e8d4bc516e82c0e664ce51ff1eaad44ee19f19b7
Nayomekaia <nayomedoelwijt@gmail.com>
2026-05-29 09:24:49 +0200

    nav desktop styles

--- src/lib/organisms/NavBottom.svelte
+++ src/lib/organisms/NavBottom.svelte
@@ -94,3 +94,98 @@
 </nav>
 
 <style>
+  /* Algemene navigatie styles */
+  .desktop-nav {
+    display: flex;
+    justify-content: center;
+    margin: 0 auto;
+    border-radius: 5px;
+    background-color: black;
+    padding: 1rem;
+    width: 1000px;
+    gap: 5rem;
+    margin-top: 2rem;
+  }
+
+  li {
+    display: flex;
+    flex-direction: row;
+    list-style: none;
+    text-transform: uppercase;
+  }
+
+  .menu-button {
+    text-decoration: none;
+    position: relative;
+    transition: all 0.5s;
+    color: var(--primary-white);
+  }
+
+  /* hover effect */
+  .menu-button:hover {
+    color: var(--primary-white);
+    text-shadow:
+      0 0 10px var(--brown-300),
+      0 0 20px var(--blue-100),
+      0 0 40px var(--primary-blue);
+  }
+
+  /* active state effect */
+  .menu-button.active {
+    text-shadow:
+      0 0 10px var(--brown-300),
+      0 0 20px var(--blue-100),
+      0 0 40px var(--primary-blue);
+  }
+
+  .menu-button.active:hover {
+    text-shadow:
+      0 0 10px var(--brown-300),
+      0 0 20px var(--blue-100),
+      0 0 40px var(--primary-blue);
+  }
+
+  details {
+    position: relative;
+    font-family: var(--font-body);
+    color: var(--primary-white);
+  }
+
+  .dropdown {
+    position: absolute;
+    top: 100%;
+    left: 0;
+    margin-top: 2rem;
+    list-style: none;
+    padding: 1.5em;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+
+  details:not([open]) .dropdown {
+    display: none;
+  }
+
+  summary {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    cursor: pointer;
+    list-style: none;
+    gap: 1rem;
+  }
+
+  summary::-webkit-details-marker {
+    display: none;
+  }
+
+  summary::after {
+    content: "▾";
+    transition: transform 0.2s ease;
+  }
+
+  details[open] summary::after {
+    transform: rotate(180deg);
+  }
+</style>
