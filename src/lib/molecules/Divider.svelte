6a79ebc4c39b025fd7d12a493beb4182f3877468
Nayomekaia <nayomedoelwijt@gmail.com>
2026-05-29 09:25:10 +0200

    divider component

---
+++ src/lib/molecules/Divider.svelte
@@ -0,0 +1,13 @@
+<div class="divider"></div>
+
+<style>
+.divider {
+    width: 100%;
+    height: 1px;
+    margin-bottom: 2rem;
+    margin-top: 1rem;
+    background: white;
+    opacity: 0.5;
+    margin: 1rem 0;
+}
+</style>
