b4f68a9423a034e252c869479a33664949bc370b
Nayomekaia <nayomedoelwijt@gmail.com>
2026-05-31 19:32:03 +0200

    Import Three.js and logo config
    #42

---
+++ src/lib/three-logo/logoObjects.js
@@ -0,0 +1,10 @@
+import * as THREE from "three";
+import {
+    GLOBAL_SCALE,
+    MODEL_BASE_SIZE,
+    SEPARATOR_Z_OFFSET,
+    LETTER_SCALE_MAP,
+    LETTER_THICKNESS_MAP,
+    RENDER_ORDER,
+} from "./logoConfig.js";
+
