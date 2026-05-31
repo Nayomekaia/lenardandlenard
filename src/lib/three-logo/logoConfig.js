bf9c0d101fb742a538207fde1cbbcac375ed3d41
Nayomekaia <nayomedoelwijt@gmail.com>
2026-05-31 19:12:58 +0200

    Add base logo config settings
    #42

---
+++ src/lib/three-logo/logoConfig.js
@@ -0,0 +1,22 @@
+// =====================================================
+// CONFIG
+// =====================================================
+export const GLOBAL_SCALE = 1;
+export const MODEL_BASE_SIZE = 1.8;
+export const SEPARATOR_Z_OFFSET = 0.4;
+
+export const CAMERA_Z = {
+    desktop: 20,
+    tablet: 45,
+    mobile: 50,
+    smallMobile: 60,
+    zoomAmount: 12,
+};
+
+export const RENDER_ORDER = {
+    normal: 1,
+    topLetter: 999,
+    activeTop: 9999,
+    separator: 10000,
+};
+
