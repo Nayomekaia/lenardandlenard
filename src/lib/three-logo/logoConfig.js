43971d8fe04fd25b640bca1bcc3fd4d44c954485
Nayomekaia <nayomedoelwijt@gmail.com>
2026-05-31 19:14:24 +0200

    Add letter model config
    #42

--- src/lib/three-logo/logoConfig.js
+++ src/lib/three-logo/logoConfig.js
@@ -30,3 +30,31 @@ export const TIMELINE = {
     phase7End: 3.3,
 };
 
+export const LETTER_SCALE_MAP = {
+    A: 1.19,
+    1: 1.5,
+    2: 1.1,
+};
+
+export const LETTER_THICKNESS_MAP = {
+    "|": { x: 2.6, y: 1, z: 1 },
+};
+
+export const LETTERS = [
+    { id: "L", file: "L.glb", x: -12 },
+    { id: "E", file: "E.glb", x: -10 },
+    { id: "N", file: "N.glb", x: -8 },
+    { id: "A", file: "A.glb", x: -6 },
+    { id: "R", file: "R.glb", x: -4 },
+    { id: "D", file: "D.glb", x: -2 },
+
+    { id: "|", file: "separator.glb", x: 0 },
+
+    { id: "D", file: "D1.glb", x: 2 },
+    { id: "2", file: "R1.glb", x: 4 },
+    { id: "A", file: "A1.glb", x: 6 },
+    { id: "N", file: "N1.glb", x: 8 },
+    { id: "E", file: "E1.glb", x: 10 },
+    { id: "L", file: "L1.glb", x: 12 },
+];
+
